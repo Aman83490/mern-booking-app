3ac993ce31e7fb53b88de20fc3df209e424b92e1
Aman83490 <101001395+Aman83490@users.noreply.github.com>
2024-07-12 10:58:54 +0530

    added search feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,19 @@ import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
 import cookieParser from "cookie-parser"
 import path from "path"
+import {v2 as cloudinary} from 'cloudinary'
+import myHotelRoutes from "./routes/my-hotels"
+import hotelRoutes from "./routes/hotels"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+const corsOptions = {
+  origin: 'http://localhost:5173', // Specify the allowed origin
+  credentials: true, // Allow credentials
+};
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -13,16 +26,22 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials:true
-}
+app.use(cors(
+    corsOptions
+
 ))
 
 app.use(express.static(path.join(__dirname,"../../frontend/dist")))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
+app.use("/api/my-hotels",myHotelRoutes)
+app.use("/api/hotels",hotelRoutes)
+
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+
 
 //app.get("/api/test", async(req: Request, res:Response)=>{
 //    res.json({message:"Hello from express"})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,15 @@ import {
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () =>{
+  const {isLoggedIn } = useAppContext()
+
   return(
     <Router>
       <Routes>
@@ -18,11 +25,36 @@ const App = () =>{
       </Layout>  } />
       <Route path="/search" element={
         <Layout>
-        <p>Search Page</p>
+          <Search />
       </Layout>
       } />
         <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="/sign-in" element={<Layout><SignIn /></Layout>}/>
+
+         {isLoggedIn && <>
+         <Route path="/add-hotel"
+         element={
+          <Layout>
+            <AddHotel />            
+          </Layout>
+         }
+         />
+         <Route path="/edit-hotel/:hotelId"
+         element={
+          <Layout>
+            <EditHotel />            
+          </Layout>
+         }
+         />
+         <Route path="/my-hotels"
+         element={
+          <Layout>
+            <MyHotels />            
+          </Layout>
+         }
+         />
+         </>}
+
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
