a5933ead47cbd999182a4c7b04880e280d0043f5
Aman83490 <101001395+Aman83490@users.noreply.github.com>
2024-07-19 11:44:23 +0530

    Adding some chnages from AppContext

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -4,7 +4,8 @@ import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
 import { loadStripe, Stripe } from "@stripe/stripe-js";
 
-const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || "";
+//const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || "";
+const STRIPE_PUB_KEY = 'pk_test_51PcLclRx2Bbg4XKhuNBWIC0RH8Uv1tkWCeJUL4Oj1OPQmTVgI0fTbccnvdHwUMTeS1WwBrmCw0Yc60RekJDkniZ500UIFnKbNv';
 
 type ToastMessage = {
     message: string,
